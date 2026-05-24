b50960f5d8a60d1f3cd106a44b9097f83a10fcb3
manfogc <alejandro.non2207@gmail.com>
2026-05-24 14:18:50 -0500

    Segundo commit de programacion lider programador del proyecto Alejando Escobar

---
+++ SIGC_SrvReservas.py
@@ -0,0 +1,2 @@
+import tkinter as tk # Importamos tkinter para la interfaz gráfica
+from tkinter import ttk, messagebox, simpledialog # ttk para combobox, messagebox para diálogos, simpledialog para pedir cantidad
